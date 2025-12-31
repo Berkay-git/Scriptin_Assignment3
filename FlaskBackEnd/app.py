91cc4bf057a9b109fa699c3eb8f32eeb28d576f7
Berkay-git <berkayceylan13@gmail.com>
2025-12-31 03:05:34 +0200

    app

--- FlaskBackEnd/app.py
+++ FlaskBackEnd/app.py
@@ -1,33 +1,27 @@
 from flask import *
 
-app = Flask(__name__, template_folder='Front_End/templates', static_folder='Front_End/static')
+app = Flask(__name__, template_folder='../Front_End/templates', static_folder='../Front_End/static')
 app.secret_key = "123"
-
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
-def loginscreen():
-    return render_template("login.html")
-
-@app.route("/register")
-def registerscreen():
+@app.route("/register",methods = ["GET","POST"])
+def register():
     return render_template("register.html")
 
 
-
+@app.route("/login")
+def loginscreen():
+    return render_template("login.html")
 
 
 @app.route("/logout")
 def logout():
     session.pop("username", None)
     return redirect(url_for('index'))  # redirects to index function above.
-
-
-
 
 if __name__ == "__main__":
     app.run()
