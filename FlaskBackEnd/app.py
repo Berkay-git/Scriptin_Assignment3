fe65ecef5f6bbb91f5f9d7476eb890aa00f53a0f
Murat Güneş <muratdorukgunes@gmail.com>
2025-12-31 03:13:20 +0200

    murat1
    edit

--- FlaskBackEnd/app.py
+++ FlaskBackEnd/app.py
@@ -8,20 +8,40 @@ app.secret_key = "123"
 def index():
     return render_template("index.html")
 
-@app.route("/register",methods = ["GET","POST"])
-def register():
+@app.route("/login", methods=["POST", "GET"])
+def loginscreen():
+    if request.method == "POST":  # if method is post, it means form in login.html is submitted, check information.
+        username = request.form["username"]
+        password = request.form["password"]
+        conn = sqlite3.connect("school.db")
+        c = conn.cursor()
+        c.execute("SELECT * FROM USER WHERE username=? AND password=?", (username, password))
+        row = c.fetchone()
+        conn.close()
+        if row != None:
+            session["username"] = username
+
+        return redirect(url_for("index"))
+
+    else:  # if method is get, show login.html.
+        return render_template("login.html")
+
+
+@app.route("/register")
+def registerscreen():
     return render_template("register.html")
 
 
-@app.route("/login")
-def loginscreen():
-    return render_template("login.html")
+
 
 
 @app.route("/logout")
 def logout():
     session.pop("username", None)
     return redirect(url_for('index'))  # redirects to index function above.
+
+
+
 
 if __name__ == "__main__":
     app.run()
